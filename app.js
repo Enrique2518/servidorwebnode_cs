f06959f296de64d037509b2d12d308cd00a5a53f
Enrique Muñoz-Torrero <152556786+Enrique2518@users.noreply.github.com>
2025-10-14 16:55:29 +0000

    First Hola Mundo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req, res)=>{
-    res.send(<h1>Hola Mundo</h1>)
+    res.send("<h1>Hola Mundo</h1>")
 } )
 
 module.exports = app
